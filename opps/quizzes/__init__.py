9282de2157cd41d071711be2ab8ff42f7d803c15
Davi Oliveira <arlocm@gmail.com>
2014-05-26 11:41:41 -0300

    revert copyright changes

--- opps/quizzes/__init__.py
+++ opps/quizzes/__init__.py
@@ -10,8 +10,8 @@ __version__ = ".".join(map(str, VERSION))
 __status__ = "Development"
 __description__ = u"quizzes App for Opps CMS"
 
-__author__ = u"YACOWS"
+__author__ = u"Bruno Rocha"
 __credits__ = []
-__email__ = u""
+__email__ = u"rochacbruno@gmail.com"
 __license__ = u""
-__copyright__ = u"Copyright 2014, YACOWS"
+__copyright__ = u"Copyright 2013, Bruno Rocha"
